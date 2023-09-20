b16f14082d585a0565b5e112d9d1015470894487
GA <55400003+github8585@users.noreply.github.com>
2023-09-20 22:52:11 +0100

    Update milestone_2.py
    refactor: Improve code readability and variable naming
    
    In this commit:
    - Renamed variables for better clarity and understanding.
    - Utilized meaningful names for variables 'available_words' and 'selected_word'.
    - Used a clear condition for validating the input letter length.
    
    
    File:
    - milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,15 @@
 import random
 
-word_list = ['orange', 'apple', 'pear', 'banana', 'kiwi']
-print(word_list)
+available_words = ['orange', 'apple', 'pear', 'banana', 'kiwi']
+print(available_words)
 
-word = random.choice(word_list)
+selected_word = random.choice(available_words)
 
-print(word)
+print(selected_word)
 
 guess = input("Enter a letter: ")
 
-if guess in word == len.word:
+if guess in selected_word and len(guess) == 1:
     print("Good guess!")
 else:
     print("Oops! That is not a valid input.")
