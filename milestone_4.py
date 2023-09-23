48c44db36e7fa92e810cefb60c046433a2500b3d
GA <55400003+github8585@users.noreply.github.com>
2023-09-23 14:49:32 +0100

    Update milestone_4.py
    Refactor Summary
    1.Modular Approach: Each piece of functionality has been assigned to individual methods like reveal_letter, reduce_life, and is_invalid_guess to ensure each method does one thing and does it well.
    
    2.Improved Readability: Added method names are more descriptive and reflect the action performed by the method, making the code easier to read and understand.
    
    3.Enhanced Maintainability: With a more modular approach, making changes or updates to specific functionalities becomes easier, as each functionality is encapsulated within its method.

--- milestone_4.py
+++ milestone_4.py
@@ -10,36 +10,43 @@ class Hangman:
         self.list_of_guesses = []
     
     def check_guess(self, guess):
+        # Convert the guessed letter to lower case
         guess = guess.lower()
         
         if guess in self.word:
-            print(f"Good guess! {guess} is in the word.")
-            
-            for index, letter in enumerate(self.word):
-                if letter == guess:
-                    self.word_guessed[index] = guess
-            
-            self.num_letters -= 1
+            self.reveal_letter(guess)
         else:
-            # Handling incorrect guesses
-            self.num_lives -= 1  # Reducing num_lives by 1
-            print(f"Sorry, {guess} is not in the word.")
-            print(f"You have {self.num_lives} lives left.")
+            self.reduce_life(guess)
+    
+    def reveal_letter(self, guess):
+        print(f"Good guess! {guess} is in the word.")
+        for index, letter in enumerate(self.word):
+            if letter == guess:
+                self.word_guessed[index] = guess
+        self.num_letters -= 1
+    
+    def reduce_life(self, guess):
+        self.num_lives -= 1
+        print(f"Sorry, {guess} is not in the word.")
+        print(f"You have {self.num_lives} lives left.")
     
     def ask_for_input(self):
         while True:
             guess = input("Please, guess a letter: ").strip().lower()
             
-            if not guess.isalpha() or len(guess) != 1:
+            if self.is_invalid_guess(guess):
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
-                self.check_guess(guess)
                 self.list_of_guesses.append(guess)
+                self.check_guess(guess)
                 break
+                
+    def is_invalid_guess(self, guess):
+        return not guess.isalpha() or len(guess) != 1
 
-# Creating an instance and calling the ask_for_input method to test the code
+
 if __name__ == "__main__":
     word_list = ["apple", "banana", "cherry"]
     game = Hangman(word_list)
