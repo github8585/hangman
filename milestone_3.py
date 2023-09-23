fbc91a48b011127df5f6bf474aef5992c3e5887c
GA <55400003+github8585@users.noreply.github.com>
2023-09-23 14:30:47 +0100

    Update milestone_3.py
    REFACTORED

--- milestone_3.py
+++ milestone_3.py
@@ -1,42 +1,23 @@
-while True:
-    guess = input("Guess a letter: ")
-    if guess.isalpha == len(guess):
-        break # break out of the loop
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
-    if guess in len(guess):
-        print("Good guess! {guess} is in the word")
-    else:
-        print("Sorry, {guess} is not in the word. Try again")
+def is_letter_in_word(letter, word):
+    letter = letter.lower()
+    return letter in word
 
-# Step 1: Define the check_guess function
-def check_guess(guess, word):
-    # Step 2: Convert the guess to lower case
-    guess = guess.lower()
-    
-    # Step 3: Check if the guess is in the word
-    if guess in word:
-        return True
-    else:
-        return False
-
-# Step 1: Define the ask_for_input function
-def ask_for_input(word):
-    # Step 2: Move the input validation code here
+def get_validated_user_input():
     while True:
-        guess = input("Guess a letter: ").lower()
-        if len(guess) != 1 or not guess.isalpha():
-            print("Please enter a single letter.")
+        user_input = input("Guess a letter: ").lower()
+        if len(user_input) == 1 and user_input.isalpha():
+            return user_input
         else:
-            break
+            print("Please enter a single alphabetical character.")
 
-    # Step 3: Call the check_guess function to check if the guess is in the word
-    if check_guess(guess, word):
-        print(f"{guess} is in the word!")
-    else:
-        print(f"{guess} is not in the word.")
+def play_guessing_game(target_word):
+    while True:
+        guessed_letter = get_validated_user_input()
+        if is_letter_in_word(guessed_letter, target_word):
+            print(f"Good guess! {guessed_letter} is in the word.")
+        else:
+            print(f"Sorry, {guessed_letter} is not in the word. Try again.")
 
-# Step 4: Call the ask_for_input function to test the code
-word = "example"  # Replace with your word
-ask_for_input(word)
-
+if __name__ == "__main__":
+    word_to_guess = "example"
+    play_guessing_game(word_to_guess)
