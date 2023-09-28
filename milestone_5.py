64f1f92bd6fc1cf70bc18344ad0849a2f9e3c87a
guled8585 <guledts@gmail.com>
2023-09-28 19:54:25 +0100

    full hangman edition

--- milestone_5.py
+++ milestone_5.py
@@ -1,33 +1,73 @@
+import random
+
+
 class Hangman:
-    def __init__(self, word_list, num_lives):
-        # Assuming your Hangman class has an initializer that takes a word list and num_lives as parameters
-        pass  # Replace with the actual code
+    def __init__(self, word_list: list[str], num_lives: int = 5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for _ in self.word]
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+        self.num_letters = len(set(self.word))
+
+    def check_guess(self, guess: str) -> None:
+        guess = guess.lower()
+
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+            for index, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[index] = guess
+
+            self.num_letters -= 1
+        else:
+            # Handling incorrect guesses
+            self.num_lives -= 1  # Reducing num_lives by 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self) -> None:
+        while True:
+            guess = input("Please, guess a letter: ").strip().lower()
+
+            if not guess.isalpha() or len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
     
-    def ask_for_input(self):
-        # Assuming this is a method that handles user input and game logic
-        pass  # Replace with the actual code
+    def is_game_over(self) -> bool:
+        return self.num_lives == 0
     
-    # Add other methods if necessary
+    def has_won(self) -> bool:
+        return self.num_letters == 0
+    
+    def display_game_state(self) -> None:
+        print(' '.join(self.word_guessed))
+        print("List of guesses:", ', '.join(self.list_of_guesses))
+        print(f"You have {self.num_lives} lives left.\n")
 
 
-def play_game(word_list):
+def play_game(word_list: list[str]) -> None:
     num_lives = 5
     game = Hangman(word_list, num_lives)
-    num_letters = 1  # Initialize with a value greater than 0 to start the game loop
-    
+
     while True:
-        if num_lives == 0:
-            print('You lost!')
-            break  # Exit the loop
-        elif num_letters > 0:
-            game.ask_for_input()  # Assuming this method will also update num_lives and num_letters accordingly
+        game.display_game_state()
+
+        if game.is_game_over():
+            print(f'You lost! The word was {game.word}.')
+            break
+        elif not game.has_won():
+            game.ask_for_input()
         else:
             print('Congratulations. You won the game!')
-            break  # Exit the loop
+            break
 
-    # Optionally, add a replay feature here if you want
-        
 
-# Replace with your actual word list
-word_list = ['apple', 'banana', 'cherry']
-play_game(word_list)
+if __name__ == "__main__":
+    word_list = ['apple', 'banana', 'cherry']
+    play_game(word_list)
